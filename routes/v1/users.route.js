4aa9c2ec74124945a517999b2051eddb79c38d30
rafiulaanam <rafiulaanam@gmail.com>
2023-03-08 22:39:38 +0600

    route update

--- routes/v1/users.route.js
+++ routes/v1/users.route.js
@@ -85,6 +85,25 @@ router
 .patch(usersControllers.updateUser);
 
 
+  router.route("/bulk-update")
+/**
+   * @api {post} /tools save a tool
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+.patch(usersControllers.updateUsers);
+
+
   router.route("/delete")
 /**
    * @api {post} /tools save a tool
